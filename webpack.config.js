42f91db6d9897c6e313d609308e6424964cad0c7
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-12-04 09:32:37 +0100

    relying on html-webpack-plugin for bundle injecting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    // what the bundle will contain
-    entry: './app/index.js',
-    // where the bundle will be located
-    output: {
-        path: path.resolve('dist'),
-        filename: 'bundle.js'
-    },
-    // how the bundle is built (transpiling js, etc)
-    module: {
-        loaders: [{
-            test : /\.js?/,
-            include : path.resolve('./app'),
-            loader : 'babel-loader',
-            exclude: /node_modules/
-        }]
-    },
-    plugins: []
+  // what the bundle will contain
+  entry: './app/index.js',
+  // where the bundle will be located
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  // how the bundle is built (transpiling js, etc)
+  module: {
+    loaders: [{
+      test : /\.js?/,
+      include : path.resolve('./app'),
+      loader : 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './index.html',
+    filename: 'index.html',
+    inject: 'body'
+  })]
 }
