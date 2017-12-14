9ac03046eb3504568eb3df75399e43f3d9736d8f
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-12-14 11:47:20 +0100

    adding runtime checking for CurrencyItem props.

--- src/app/components/CurrencyItem.js
+++ src/app/components/CurrencyItem.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const CurrencyItem = props => <li><Link to={`/currencies/${props.id}`}>{props.display_name}</Link></li>
+const CurrencyItem = ({id, display_name}) => <li><Link to={`/currencies/${id}`}>{display_name}</Link></li>
+
+CurrencyItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  display_name: PropTypes.string.isRequired
+}
 
 export default CurrencyItem
